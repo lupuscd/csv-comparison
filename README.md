6f427d8170c346e55a978371bcba05097a175c42
phoenixgod17 <xrhstostagikas.phys@gmail.com>
2022-12-12 23:55:59 +0200

    Fixed various issues.

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-Tool to read csv files. Find the common elements and put everything in a list.
+Tool to read csv files from a folder and keep certain columns from each,
+based on the column title. (here we need the name and wegiht of each stock)
+
+Then create a new csv file keeping the values from the columns of each csv.
---
+++ csv_comp.py
@@ -0,0 +1,29 @@
+import pandas as pd
+import os
+
+path = '/Users/invictus/Programing/Projects/csv comparison/new csv/'
+csv_path = '/Users/invictus/Programing/Projects/csv comparison/etf holdings/'
+filenames = [ f for f in os.listdir(csv_path) if f.endswith('.csv')]
+weight_list = ['Weight %', 'Weight (%)', 'Weight', '% of net assets', 'Weighting']
+name_list = ['Security', ' Security Description', 'Holding', 'Name', 'Security Name']
+
+dataframes = []
+
+for f in filenames:
+    rpath = os.path.join(path, f)
+    data = pd.read_csv(rpath,sep=';', on_bad_lines='skip', engine='python')
+    existing_names = [clmn for clmn in name_list if clmn in data.columns]
+    existing_weights = [wght for wght in weight_list if wght in data.columns]
+    str_names = ''.join(existing_names)
+    str_weights = ''.join(existing_weights)
+    rdata = data[[str_names, str_weights]]
+    rdata.columns = rdata.iloc[0]
+    rdata.drop(0)
+    dataframes.append(rdata)
+
+#colmns = ['Name', 'Weight (%)']
+fdata = pd.concat(dataframes, ignore_index=True)
+
+#data['Name'] = data['Name'].str.lower()
+fdata.to_csv('/Users/invictus/Programing/Projects/csv comparison/data/combined_data.csv',
+ index = False)
